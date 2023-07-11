9dfe66723c937e39d207bb4d84e28dfd3635bd69
Oliver Tworkowski <olitworkowski@gmail.com>
2023-07-11 16:40:54 +0200

    more fixes and initial controller GO for Gaze Interaction

--- Assets/Scripts/CustomSunLight.cs
+++ Assets/Scripts/CustomSunLight.cs
@@ -8,58 +8,39 @@ using LightType = UnityEngine.LightType;
 [ExecuteInEditMode]
 public class CustomSunLight : MonoBehaviour
 {
-    [SerializeField]
-    private FalloffType falloffType;
+    public FalloffType fallOff;
 
     public void OnEnable()
     {
-        Lightmapping.RequestLightsDelegate lightsDelegate = (Light[] requests, NativeArray<LightDataGI> lightsOutput) =>
+        Lightmapping.RequestLightsDelegate testDel = (Light[] requests, Unity.Collections.NativeArray<LightDataGI> lightsOutput) =>
         {
+
+            DirectionalLight dLight = new DirectionalLight();
+            PointLight point = new PointLight();
+            SpotLight spot = new SpotLight();
+            RectangleLight rect = new RectangleLight();
+            LightDataGI ld = new LightDataGI();
+
             for (int i = 0; i < requests.Length; i++)
             {
                 Light l = requests[i];
-                LightDataGI ld = new LightDataGI();
-
                 switch (l.type)
                 {
-                    case LightType.Directional:
-                        DirectionalLight dLight = new DirectionalLight();
-                        LightmapperUtils.Extract(l, ref dLight);
-                        ld.Init(ref dLight);
-                        break;
-                    case LightType.Point:
-                        PointLight point = new PointLight();
-                        LightmapperUtils.Extract(l, ref point);
-                        ld.Init(ref point);
-                        break;
-                    case LightType.Spot:
-                        SpotLight spot = new SpotLight();
-                        LightmapperUtils.Extract(l, ref spot);
-                        ld.Init(ref spot);
-                        break;
-                    case LightType.Area:
-                        RectangleLight rect = new RectangleLight();
-                        LightmapperUtils.Extract(l, ref rect);
-                        ld.Init(ref rect);
-                        break;
-                    case LightType.Disc:
-                        DiscLight disc = new DiscLight();
-                        LightmapperUtils.Extract(l, ref disc);
-                        ld.Init(ref disc);
-                        break;
-                    default:
-                        ld.InitNoBake(l.GetInstanceID());
-                        break;
+                    case UnityEngine.LightType.Directional: LightmapperUtils.Extract(l, ref dLight); ld.Init(ref dLight); break;
+                    case UnityEngine.LightType.Point: LightmapperUtils.Extract(l, ref point); ld.Init(ref point); break;
+                    case UnityEngine.LightType.Spot: LightmapperUtils.Extract(l, ref spot); ld.Init(ref spot); break;
+                    case UnityEngine.LightType.Area: LightmapperUtils.Extract(l, ref rect); ld.Init(ref rect); break;
+                    default: ld.InitNoBake(l.GetInstanceID()); break;
                 }
 
-                ld.cookieID = l.cookie?.GetInstanceID() ?? 0;
-                ld.falloff = falloffType;
+                ld.falloff = fallOff;
                 lightsOutput[i] = ld;
             }
         };
 
-        Lightmapping.SetDelegate(lightsDelegate);
+        Lightmapping.SetDelegate(testDel);
     }
+
 
     void OnDisable()
     {
